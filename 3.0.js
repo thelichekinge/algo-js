3e861523c13107338fa92f274d0c69d3c9995578
thelichekinge <49574788+thelichekinge@users.noreply.github.com>
2025-11-21 11:12:40 +0100

    Les pseudos

---
+++ 3.0.js
@@ -0,0 +1,24 @@
+const classLearners = [
+  "thelichkinge",
+  "ethan",
+  "Hetaska",
+  "Enrique2802",
+  "Trollman341",
+  "Kisame25021990",
+  "Léandre Mpore-Ghozzo",
+  "Abdoul",
+  "Aucouturier Jason",
+  "xam1712",
+  "Dodo-31",
+  "MattWare",
+  "Thomas",
+  "lalbecade",
+  "Gaetan",
+  "Dimitri Gillain",
+  "TyberCusa",
+  "Johan"
+];
+
+for (const learner of classLearners) {
+  console.log(learner);
+}
